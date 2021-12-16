635bc25435c2797783247a0c6a5089680126df3c
FreddyJS <sebashgt@gmail.com>
2021-12-16 19:54:36 +0100

    Single header file

--- linkedlist/LinkedList.hpp
+++ linkedlist/LinkedList.hpp
@@ -13,7 +13,71 @@
 #include <iostream>
 #include <exception>
 
-#include <LinkedListItem.hpp>
+/*****************  LinkedListItem  *****************/
+/**
+ * @brief A class that represents a node in a linked list.
+ * 
+ * @tparam T  The type of the data stored in the node.
+ */
+template <class T>
+class LinkedListItem {
+
+    public:
+        T data; /**< The data stored in the node. */
+        LinkedListItem<T>* next; /**< The next node in the linked list. */
+        LinkedListItem<T>* previous; /**< The previous node in the linked list. */
+
+        LinkedListItem() {
+            this->next = 0;
+            this->previous = 0;
+        }
+
+        LinkedListItem(T data) {
+            this->next = 0;
+            this->previous = 0;
+            this->data = data;
+        }
+
+        ~LinkedListItem() { }
+
+        T getData();
+        T* getDataPtr();
+        void setData(T data);
+
+};
+
+/**
+ * @brief Returns the data stored in the node.
+ * 
+ * @tparam T The type of the data stored in the node.
+ * @return T The data stored in the node.
+ */
+template <class T>
+T LinkedListItem<T>::getData() {
+    return this->data;
+}
+
+/**
+ * @brief Returns a pointer to the data stored in the node.
+ * 
+ * @tparam T The type of the data stored in the node.
+ * @return T* A pointer to the data stored in the node.
+ */
+template <class T>
+T* LinkedListItem<T>::getDataPtr() {
+    return &(this->data);
+}
+
+/**
+ * @brief Sets the data stored in the node.
+ * 
+ * @tparam T The type of the data stored in the node.
+ * @param data The data to be stored in the node.
+ */
+template <class T>
+void LinkedListItem<T>::setData(T data) {
+    this->data = data;
+}
 
 /**
  * @brief Custom exception class. Used to throw exceptions.
@@ -48,6 +112,7 @@ class LinkedListIterator
         void operator++() {p = p->next;}
 };
 
+/*****************  LinkedList  *****************/
 /**
  * @brief Doubly linked list implementation.
  * 
